42764910a4bfea04e526001f7a42bc4e041703e1
Sanika P <Sanika.P@soti.net>
2023-09-26 13:15:34 +0530

    time in surgery

--- src/app/view-todays-surgery/view-todays-surgery.component.ts
+++ src/app/view-todays-surgery/view-todays-surgery.component.ts
@@ -9,15 +9,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./view-todays-surgery.component.css']
 })
 export class ViewTodaysSurgeryComponent {
+  startTime:string;
+  endTime:string;
+
+  convertDecimalToTime(decimalValue: number): string {
+    let hours = Math.floor(decimalValue);
+    const minutes = Math.round((decimalValue - hours) * 60);
+    let period = 'AM';
+  
+    if (hours >= 12) {
+      period = 'PM';
+      if (hours > 12) {
+        hours -= 12;
+      }
+    }
+  
+    if (hours === 0) {
+      hours = 12;
+    }
+  
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${period}`;
+  }
+  
   TodaysSurgeryList:SurgeryModel[]|null=null;
 
   constructor(private surgeryService:SurgeryService,private router: Router){
     surgeryService.getAllSurgeriesForToday().subscribe(
       surgeries => {this.TodaysSurgeryList = surgeries;
-      console.log(this.TodaysSurgeryList);
       }
     );
   }
+
+  
   editSurgery(surgery: SurgeryModel): void {
     
     this.router.navigate(['/updateSurgery', surgery.SurgeryId]);
