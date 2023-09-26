0a4750915a194ad0806d2d8b1a25319b4e50659e
Sreekanth Kartha <Sreekanth.Kartha@soti.net>
2023-09-26 16:14:55 +0530

    pagenotfound

---
+++ src/app/pagenot-found-error/pagenot-found-error.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenot-found-error',
+  templateUrl: './pagenot-found-error.component.html',
+  styleUrls: ['./pagenot-found-error.component.css']
+})
+export class PagenotFoundErrorComponent {
+
+}
