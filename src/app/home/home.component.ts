3dc779b46dbee0b860f22162b56358622b88150f
Sanika P <Sanika.P@soti.net>
2023-09-26 10:14:45 +0530

    home screen carousal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  images: string[] = [
+    '../assets/images/image3.jpg',
+    '../assets/images/image1.jpg',
+    '../assets/images/image2.jpg',
+    
+  ];
 
+  currentIndex = 0;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.nextImage();
+    }, 3000); // Change image every 3 seconds (adjust as needed)
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
